511a276d925ac0b48a3d5e7cbd5bcbc5ba351113
nval235 <develop235@gmx.co.uk>
2025-01-12 00:11:22 +0000

    commit 20250112

---
+++ demo_20250111/src/main/java/com/example/demo/Demo20250111Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20250111Application {
+
+	public static void main(String[] args) {
+		System.out.println("~~~~~~~~~~~~~~");
+		SpringApplication.run(Demo20250111Application.class, args);
+	}
+
+}
